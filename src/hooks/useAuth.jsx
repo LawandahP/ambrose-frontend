983d5b740be5cd37ae5c820572da88fe6c02d7b9
Githaiga <pythongeeko@gmail.com>
2024-01-23 22:45:13 +0300

    Set up user authentication context and services

---
+++ src/hooks/useAuth.jsx
@@ -0,0 +1,52 @@
+/* eslint-disable react/prop-types */
+// src/context/UserContext.jsx
+import { createContext, useState } from 'react';
+import { exchangeCodeForAccessToken, fetchUserDetails } from '../services/authService';
+
+export const UserContext = createContext(null);
+
+export const UserAuth = ({ children }) => {
+  const [userDetails, setUserDetails] = useState(() => {
+    const savedUserDetails = localStorage.getItem('userDetails');
+    return savedUserDetails ? JSON.parse(savedUserDetails) : null;
+  });
+
+  const authenticate = async (provider, accessToken) => {
+    try {
+      const userDetails = await fetchUserDetails(provider, accessToken);
+      console.log("userDetails", userDetails);
+      localStorage.setItem('userDetails', JSON.stringify(userDetails));
+      setUserDetails(userDetails);
+    } catch (error) {
+      console.error('Authentication failed:', error);
+    }
+  };
+
+  const exchangeCodeForToken = async (code) => {
+    try {
+      const accessToken = await exchangeCodeForAccessToken(code);
+      return accessToken;
+    } catch (error) {
+      console.error('GitHub code exchange failed:', error);
+      throw error;
+    }
+  };
+
+  const logout = () => {
+    localStorage.removeItem('userDetails');
+    setUserDetails(null);
+  };
+
+  const contextValue = {
+    userDetails,
+    authenticate,
+    exchangeCodeForToken,
+    logout
+  };
+
+  return (
+    <UserContext.Provider value={contextValue}>
+      {children}
+    </UserContext.Provider>
+  );
+};
---
+++ src/services/authService.jsx
@@ -0,0 +1,52 @@
+import axios from 'axios';
+
+// Function to fetch user details from the OAuth provider
+export const fetchUserDetails = async (provider, accessToken) => {
+  let userInfoUrl;
+
+  // Determine the URL based on the provider
+  switch (provider) {
+    case 'github':
+      userInfoUrl = 'https://api.github.com/user';
+      break;
+    // Add more cases for other providers
+    // case 'google':
+    //   userInfoUrl = 'https://www.googleapis.com/oauth2/v3/userinfo';
+    //   break;
+    // case 'facebook':
+    //   userInfoUrl = 'https://graph.facebook.com/me?fields=id,name,email,picture';
+    //   break;
+    default:
+      throw new Error('Unsupported provider');
+  }
+
+  // Make an HTTP GET request to the provider's user info URL
+  const response = await axios.get(userInfoUrl, {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+
+  // Return the user data
+  return response.data;
+};
+
+// request for access token using code gotten from query param 
+export const exchangeCodeForAccessToken = async (code) => {
+  try {
+    const response = await axios.post(import.meta.env.VITE_GITHUB_OAUTH, {
+      client_id: import.meta.env.VITE_GITHUB_CLIENT_ID,
+      client_secret: import.meta.env.VITE_GITHUB_CLIENT_SECRET,
+      code: code,
+      redirect_uri: import.meta.env.VITE_GITHUB_REDIRECT_URI
+    });
+    console.log("response", response)
+    return response.data.access_token;
+  } catch (error) {
+    if (error.response && error.response.data && error.response.data.detail) {
+      throw new Error(error.response.data.detail);
+    } else {
+      throw new Error('Failed to exchange code for access token');
+    }
+  }
+};
