ddefa60063919be42e12a3ed83c89bf4d17c0994
Githaiga <pythongeeko@gmail.com>
2024-01-26 09:01:18 +0300

    delete actions

--- src/services/oauthConfig.jsx
+++ src/services/oauthConfig.jsx
@@ -1,23 +1,23 @@
 export const googleConfig = {
-    clientId: "YOUR_GOOGLE_CLIENT_ID",
-    redirectUri: "YOUR_REDIRECT_URI",
-    authorizationUrl: "https://accounts.google.com/o/oauth2/v2/auth",
-    scope: "openid profile email",
-  };
+  clientId: "YOUR_GOOGLE_CLIENT_ID",
+  redirectUri: "YOUR_REDIRECT_URI",
+  authorizationUrl: "https://accounts.google.com/o/oauth2/v2/auth",
+  scope: "openid profile email",
+};
   
-  export const facebookConfig = {
-    clientId: "YOUR_FACEBOOK_CLIENT_ID",
-    redirectUri: "YOUR_REDIRECT_URI",
-    authorizationUrl: "https://www.facebook.com/v9.0/dialog/oauth",
-    scope: "email",
-  };
+export const facebookConfig = {
+  clientId: "YOUR_FACEBOOK_CLIENT_ID",
+  redirectUri: "YOUR_REDIRECT_URI",
+  authorizationUrl: "https://www.facebook.com/v9.0/dialog/oauth",
+  scope: "email",
+};
   
-  export const githubConfig = {
-    clientId: import.meta.env.VITE_GITHUB_CLIENT_ID,
-    redirectUri: import.meta.env.VITE_GITHUB_REDIRECT_URI,
-    authorizationUrl: "https://github.com/login/oauth/authorize",
-    scope: "user",
-  };
+export const githubConfig = {
+  clientId: import.meta.env.VITE_GITHUB_CLIENT_ID,
+  redirectUri: import.meta.env.VITE_GITHUB_REDIRECT_URI,
+  authorizationUrl: "https://github.com/login/oauth/authorize",
+  scope: "user",
+};
   
   // Add more configurations as needed
   
